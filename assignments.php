98491776bf0f8837e83638d88f5d52368d43f1dc
meganallred <meg21allred@gmail.com>
2020-09-30 11:38:14 -0400

    adding stuff

---
+++ assignments.php
@@ -0,0 +1,19 @@
+<?php
+echo "this is my assignments page"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignments</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Montserrat:wght@300&family=Sacramento&display=swap" rel="stylesheet">
+
+</head>
+<body>
+   <h1> This is my Assignments Page </h1>
+</body>
+</html>
