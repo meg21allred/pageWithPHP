3e7f0ab7ac749802f581194afdbe6ca9118211be
meganallred <meg21allred@gmail.com>
2020-10-02 09:46:28 -0400

    trying again

--- Assignments/ponder03.php
+++ Assignments/ponder03.php
@@ -6,10 +6,14 @@ $password = "c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9";
 $dbname = "d9cllninl3psq3";
 $port = "5432";
 
-// $db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
-// $db["path"] = ltrim($db["path"], "/");
+$db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
+$db["path"] = ltrim($db["path"], "/");
 
-$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+// $db = pg_connect("host=ec2-3-224-97-209.compute-1.amazonaws.com
+//                   port=5432
+//                   dbname=d9cllninl3psq3
+//                   user=rxghsggtzmiezw
+//                   password=c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9");
 
 $sql = "SELECT * FROM saleItems";
 $result = pg_query($sql);
