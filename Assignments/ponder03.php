8fb4a2ec161380d2e251312f1d038f11773824ac
meganallred <meg21allred@gmail.com>
2020-10-02 10:23:48 -0400

    adding class

--- Assignments/ponder03.php
+++ Assignments/ponder03.php
@@ -1,32 +1,57 @@
 <?php
 
-$host = "ec2-3-224-97-209.compute-1.amazonaws.com";
-$user = "rxghsggtzmiezw";
-$password = "c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9";
-$dbname = "d9cllninl3psq3";
-$port = "5432";
+// $host = "ec2-3-224-97-209.compute-1.amazonaws.com";
+// $user = "rxghsggtzmiezw";
+// $password = "c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9";
+// $dbname = "d9cllninl3psq3";
+// $port = "5432";
 
-$db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
-$db["path"] = ltrim($db["path"], "/");
+// $db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
+// $db["path"] = ltrim($db["path"], "/");
 
-// $db = pg_connect("host=ec2-3-224-97-209.compute-1.amazonaws.com
-//                   port=5432
-//                   dbname=d9cllninl3psq3
-//                   user=rxghsggtzmiezw
-//                   password=c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9");
+// // $db = pg_connect("host=ec2-3-224-97-209.compute-1.amazonaws.com
+// //                   port=5432
+// //                   dbname=d9cllninl3psq3
+// //                   user=rxghsggtzmiezw
+// //                   password=c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9");
 
-$conn = pg_connect($db["path"]);
+// $conn = pg_connect($db["path"]);
 
-$sql = "SELECT * FROM saleItems";
-$result = pg_query($sql);
+// $sql = "SELECT * FROM saleItems";
+// $result = pg_query($sql);
 
-foreach ($result as $key) {
-    echo $key['item'] . " ";
-    echo $key['id'] . " ";
-    echo $key['quan'] . " ";
-    echo $key['price'];
+// foreach ($result as $key) {
+//     echo $key['item'] . " ";
+//     echo $key['id'] . " ";
+//     echo $key['quan'] . " ";
+//     echo $key['price'];
+// }
+
+// echo "do you see a table?";
+
+class item {
+    var $id;
+    var $name;
+    var $photo;
+    var $quan;
+    var $price;
+
+    public function print_item() {
+        echo $this->name . " ";
+        echo "<image src='{$this->photo}'>" . " ";
+        echo $this->quan . " ";
+        echo "$" . $this->price;
+
+    }
+
+    
 }
 
-echo "do you see a table?";
+$item_1 = new item;
+$item_1->name = "Diapers";
+$item_1->photo = "diapers.jpg";
+$item_1->price = "9.95";
+
+$item_1->print_item();
 
  ?>
