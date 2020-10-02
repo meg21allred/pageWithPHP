c616f40e6f8a345a9ed67ba52a6eaf5628458fff
meganallred <meg21allred@gmail.com>
2020-10-02 09:41:18 -0400

    trying to query the database

--- Assignments/ponder03.php
+++ Assignments/ponder03.php
@@ -1,26 +1,25 @@
 <?php
-// Connecting, selecting database
-$db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
-$db["path"] = ltrim($db["path"], "/");
 
-// Performing SQL query
-$query = 'SELECT * FROM saleItems';
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+$host = "ec2-3-224-97-209.compute-1.amazonaws.com";
+$user = "rxghsggtzmiezw";
+$password = "c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9";
+$dbname = "d9cllninl3psq3";
+$port = "5432";
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
+// $db = parse_url(getenv("postgres://rxghsggtzmiezw:c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d9cllninl3psq3"));
+// $db["path"] = ltrim($db["path"], "/");
+
+$db = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+$sql = "SELECT * FROM saleItems";
+$result = pg_query($sql);
+
+foreach ($result as $key) {
+    echo $key['item'] . " ";
+    echo $key['id'] . " ";
+    echo $key['quan'] . " ";
+    echo $key['price'];
 }
-echo "</table>\n";
 
-// Free resultset
-pg_free_result($result);
 
-// Closing connection
-pg_close($dbconn);
-?>
+ ?>
