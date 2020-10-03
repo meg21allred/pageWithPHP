3892d716711ce0b09f20f69303ba19c57ca0c944
meganallred <meg21allred@gmail.com>
2020-10-03 15:44:04 -0400

    adding user info

--- Assignments/ponder03/checkOut.php
+++ Assignments/ponder03/checkOut.php
@@ -7,9 +7,25 @@
 </head>
 <body>
     <?php
+    SESSION_START();
+
     echo "This is the checkout page";
     ?>
-    <a href="confirm.php">Confirm Purchase</a>
+
+    <div>
+    <span>Fill out the following information: </span><br><br>
+    <form action="confirm.php" method="get">
+        <label for="first_name">First Name: </label><input type="text" name="first_name"><br>
+        <label for="last_name"></label>Last Name: <input type="text" name="last_name"><br>
+        <label for="street"></label>Street Address: <input type="text" name="street"><br>
+        <label for="city"></label>City: <input type="text" name="city"><br>
+        <label for="state"></label>State:<input type="text" name="state"><br>
+        <label for="zip"></label>Zip Code: <input type="text" name="zip"><br>
+        <input type="submit" value="Confirm Purchase">
+    </form>
+    </div>
+
+
 </body>
 </html>
 
