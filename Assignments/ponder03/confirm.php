aadd92d610e0c3f48a6aafea8f5e224ad2960dbf
meganallred <meg21allred@gmail.com>
2020-10-03 16:04:23 -0400

    adding final table

--- Assignments/ponder03/confirm.php
+++ Assignments/ponder03/confirm.php
@@ -7,7 +7,21 @@
 </head>
 <body>
     <?php
-        echo "this is the confirmation page.";
+    SESSION_START();
+
+    if(!(isset($_SESSION['cart']))) {
+        $_SESSION['cart'];
+    } // end of session cart if
+
+    $host = "ec2-3-224-97-209.compute-1.amazonaws.com";
+    $user = "rxghsggtzmiezw";
+    $password = "c2b6fe631e6453416ef8514e024fba6323e9274d3cd433dc0d6d1cb3aad789a9";
+    $dbname = "d9cllninl3psq3";
+    $port = "5432";
+
+    $db_connection = pg_connect("host=$host dbname=$dbname user=$user password=$password")
+    or die ("Could not connect to server\n");
+
 
         $first = $_GET['first_name'];
         $last = $_GET['last_name'];
@@ -17,8 +31,44 @@
         $zip = $_GET['zip'];
 
         echo "<h1>Thank you " . $first . " " . $last . "!</h1>";
-        echo "<h2>Your order will be sent to: " . 
-            $street . ", " . $city . ", " . $state . $zip . "</h1>";
+        echo "<h2>Your order of:</h2>";
+        
+        
+            echo "<table>
+                <tr>
+                    <td>Item</td>
+                    <td></td>
+                    <td>Price</td>
+                    <td>Quantity</td>
+                    <td></td>
+                    <td>SubTotal</td>
+                    </tr>";
+        
+            foreach ($_SESSION['cart'] as $key => $val) {
+                $sql = "SELECT * FROM saleItems WHERE id = '$key'";
+                $result = pg_query($db_connection, $sql) or die("cannot querey the database");
+                $row = pg_fetch_assoc($result);
+        
+                $sub = $row['price']*$val;
+                $grand += $sub;
+                echo "
+                <tr>
+                    <td>{$row['item']}</td>
+                    <td><image src='{$row['image']}' width='150px'></td>
+                    <td>$val</td>
+                ";
+            } //for each loop
+        
+        echo "<tr>
+                    <td colspan='4'>Grand Total: $ $grand</td>
+              </tr>";
+        echo "</table>";
+        
+        
+        
+        
+        echo "will be sent to: " . 
+            $street . ", " . $city . ", " . $state . " " . $zip . "</h1>";
 
     ?>
 </body>
