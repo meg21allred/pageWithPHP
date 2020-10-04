448e9ce3417628b0cd5d449c262fd39248453d71
meganallred <meg21allred@gmail.com>
2020-10-03 22:33:46 -0400

    doesn't look right

--- Assignments/ponder03/ponder03.php
+++ Assignments/ponder03/ponder03.php
@@ -15,7 +15,7 @@
        <ul class="nav">
            <a href="ponder03.php"><li>HOME</li></a>
            <a href="cart.php"><li>VIEW CART</li></a>
-           <li><?php echo "<a href='ponder03.php?clear=1'>Clear Cart</a>";?> </li>
+           <?php echo "<a href='ponder03.php?clear=1'><li>Clear Cart</li></a>";?> 
        </ul>
     </div>
 </div>
