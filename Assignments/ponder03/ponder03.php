f4b0857135cd7e98b02fb8a44ff169459f0cacca
meganallred <meg21allred@gmail.com>
2020-10-03 22:08:21 -0400

    adding logo

--- Assignments/ponder03/ponder03.php
+++ Assignments/ponder03/ponder03.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Browse</title>
     <link rel="stylesheet" href="ponder03.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
 </head>
 <body>
 <div class="header">
     <div class="inner_header">
+    <div class="logo_container"><h1>MY<span>SITE</span></h1></div>
     </div>
 </div>
 
