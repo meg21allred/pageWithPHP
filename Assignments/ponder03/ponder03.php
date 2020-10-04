9d7e797652c80c59c1ec47dd98ec8b3bdfcf600c
meganallred <meg21allred@gmail.com>
2020-10-03 22:31:25 -0400

    adding links

--- Assignments/ponder03/ponder03.php
+++ Assignments/ponder03/ponder03.php
@@ -9,8 +9,14 @@
 </head>
 <body>
 <div class="header">
-    <div class="inner_header">
-    <div class="logo_container"><h1>MY<span>BABIES</span></h1></div>
+    <div class="inner_header"></div>
+       <div class="logo_container"><h1>MY<span>BABIES</span></h1></div>
+
+       <ul class="nav">
+           <a href="ponder03.php"><li>HOME</li></a>
+           <a href="cart.php"><li>VIEW CART</li></a>
+           <li><?php echo "<a href='ponder03.php?clear=1'>Clear Cart</a>";?> </li>
+       </ul>
     </div>
 </div>
 
