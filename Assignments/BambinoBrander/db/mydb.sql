86f3f3132365d5751c1cca7e3e8c6f5396fc2dac
meganallred <meg21allred@gmail.com>
2020-10-09 14:54:26 -0400

    adding more tables

--- Assignments/BambinoBrander/db/mydb.sql
+++ Assignments/BambinoBrander/db/mydb.sql
@@ -3,14 +3,14 @@
 CREATE TABLE user_login (
     id SERIAL PRIMARY KEY,
     userName VARCHAR(255) NOT NULL,
-    userPassword VARCHAR(255) NOT NULL,
+    userPassword VARCHAR(255) NOT NULL
     
 );
 
 -- Table for girl names
 CREATE TABLE girl_names (
     id SERIAL PRIMARY KEY,
-    girl_name VARCHAR(255) NOT NULL,
+    girl_name VARCHAR(255) NOT NULL UNIQUE,
     def VARCHAR(255),
     origin VARCHAR(255)
 );
@@ -18,7 +18,7 @@ CREATE TABLE girl_names (
 -- Table for boy names
 CREATE TABLE boy_names (
     id SERIAL PRIMARY KEY,
-    boy_name VARCHAR(255) NOT NULL,
+    boy_name VARCHAR(255) NOT NULL UNIQUE,
     def VARCHAR(255),
     origin VARCHAR(255)
 );
@@ -31,6 +31,6 @@ CREATE TABLE pickedNames (
     login_id INT,
     FOREIGN KEY (picked_girl_name) REFERENCES girl_names(girl_name),
     FOREIGN KEY (picked_boy_name) REFERENCES boy_names(boy_name),
-    FOREIGN KEY (login_id) REFERENCES user_login(id),
+    FOREIGN KEY (login_id) REFERENCES user_login(id)
 );
 
