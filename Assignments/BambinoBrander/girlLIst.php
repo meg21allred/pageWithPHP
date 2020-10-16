5c3591ac5d8989ca0b0085e9bc41811106be3f02
meganallred <meg21allred@gmail.com>
2020-10-16 10:33:00 -0400

    why

--- Assignments/BambinoBrander/girlLIst.php
+++ Assignments/BambinoBrander/girlLIst.php
@@ -7,7 +7,42 @@
 </head>
 <body>
 
-<h1>this is the girl page</h1>
+<?php
+        try
+        {
+        $dbUrl = getenv('DATABASE_URL');
+        
+        $dbOpts = parse_url($dbUrl);
+        
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+        
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+        }
+        echo "<h1>Girl Names</h1><br>";
+        
+        echo "<ul>";
+
+    foreach ($db->query("SELECT * FROM girl_names") as $row)
+    {
+    echo "<li>";
+    echo "<strong>" . $row['girl_name'] . "</strong> " . "<br>     Definition: " . $row['def'] . "<br>     Origin: " . $row['origin'];
+    echo "</li>";
+    }
+
+    echo "</ul>";
+
+    ?>
     
 </body>
 </html>
--- Assignments/BambinoBrander/index.php
+++ Assignments/BambinoBrander/index.php
@@ -9,7 +9,6 @@
     <h1>Welcom to Bambino Brander</h1>
     <h2>choose a Gender</h2>
     
-    <input type="button" value="Boy" onClick="location.href='boyList.php'">
     <input type="button" value="Girl" onClick="location.href='girlList.php'">
 
     
