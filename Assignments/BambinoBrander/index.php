2d7ec6e346fa6de0be7fc5b800d6cf0a50a5ff3f
meganallred <meg21allred@gmail.com>
2020-10-16 15:25:37 -0400

    spelling errors

--- Assignments/BambinoBrander/index.php
+++ Assignments/BambinoBrander/index.php
@@ -6,8 +6,8 @@
     <title>Bambino Brander</title>
 </head>
 <body>
-    <h1>Welcom to Bambino Brander</h1>
-    <h2>choose a Gender</h2>
+    <h1>Welcome to Bambino Brander</h1>
+    <h2>Choose a Gender</h2>
     
     <input type="button" value="Boy" onClick="location.href='boyList.php?id=1'">
     <input type="button" value="Girl" onClick="location.href='girl.php?id=1'">
