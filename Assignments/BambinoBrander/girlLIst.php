8122d7021aaad37da50891307c54a14e470b9acd
meganallred <meg21allred@gmail.com>
2020-10-16 10:19:42 -0400

    test

--- Assignments/BambinoBrander/girlLIst.php
+++ Assignments/BambinoBrander/girlLIst.php
@@ -6,42 +6,8 @@
     <title>Girl Names</title>
 </head>
 <body>
-<?php
-    try
-    {
-      $dbUrl = getenv('DATABASE_URL');
-    
-      $dbOpts = parse_url($dbUrl);
-    
-      $dbHost = $dbOpts["host"];
-      $dbPort = $dbOpts["port"];
-      $dbUser = $dbOpts["user"];
-      $dbPassword = $dbOpts["pass"];
-      $dbName = ltrim($dbOpts["path"],'/');
-    
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    
-      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
-    echo "<h1>Girl Names</h1><br>";
-    
-    echo "<ul>";
 
-foreach ($db->query("SELECT * FROM girl_names") as $row)
-{
-  echo "<li>";
-  echo "<strong>" . $row['girl_name'] . "</strong> " . "<br>     Definition: " . $row['def'] . "<br>     Origin: " . $row['origin'];
-  echo "</li>";
-}
-
-echo "</ul>";
-
-    ?>
+<h1>this is the girl page</h1>
     
 </body>
 </html>
