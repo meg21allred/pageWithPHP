026790d754f4bdc59dd99bd33e9ae814757fe2d4
meganallred <meg21allred@gmail.com>
2020-10-17 12:11:06 -0400

    adding details styles

--- Assignments/BambinoBrander/details.php
+++ Assignments/BambinoBrander/details.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Girl Details</title>
+    <title>Details</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="styles.css">
 
@@ -27,13 +27,13 @@
         </div>
     </div>
 
-    <br><br><br>
-    
+    <br><br><br><br><br>
+
     <center>
         <?php
 
 
-            echo "<h1>Name Details</h1><br>";
+            echo "<h1 class='pinkText'>Name Details</h1><br>";
 
             $id = $_GET['id'];
             
@@ -50,7 +50,7 @@
                     $def = $sRow["def"];
                     $origin = $sRow["origin"];
                     echo "<li>";
-                    echo "<strong>$name</strong> <br> Definition: $def <br> Origin: $origin";
+                    echo "<strong class='girlBabyName'>$name</strong> <br><span class='details'> Definition: $def <br> Origin: $origin</span>";
                     echo "</li>";
                         
                     
