978c3ab336425968a95fc92f4f80285dc906e926
meganallred <meg21allred@gmail.com>
2020-10-22 12:51:08 -0400

    adding user insertion

--- Assignments/BambinoBrander/registrationSuccess.php
+++ Assignments/BambinoBrander/registrationSuccess.php
@@ -4,7 +4,25 @@
     $user = $_POST['userName'];
     $email = $_POST['email'];
     $password = $_POST['userPassword'];
+    $num = 0;
 
+    $checkEmail = $db->prepare("SELECT email FROM user_login Where email = :email");
+    $checkEmail->execute(array(':email' => $email));
 
-   echo "registration successful!" . $user . $email . $password;
+    while ($row = $checkEmail->fetch(PDO::FETCH_ASSOC)) {
+        $num++;
+    }
+
+    echo $num;
+
+    if($num > 0) {
+        echo "user name already taken";
+    } else {
+        $reg = $db->prepare("INSERT INTO user_login(userName, userPassword, email) 
+                            VALUES (:userName, :userPassword, :email)");
+        $reg->execute(array(':userName' => $user, 'userPassword' => $password, ':email' => $email));
+
+        echo "registration successful!" . $user . " " . $email . " " . $password;
+    }
+  
 ?>
