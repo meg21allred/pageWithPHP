d82483c262e49999f45e020a4ce61ca07c0c274e
meganallred <meg21allred@gmail.com>
2020-10-24 08:34:04 -0400

    testing addnames

--- Assignments/BambinoBrander/addName.php
+++ Assignments/BambinoBrander/addName.php
@@ -7,14 +7,14 @@
     $name;
     
     if (isset($_SESSION['userName'])) {
-        $getName = $db->prepare("SELECT boy_name FROM boy_names WHERE id = :id");
-        $getName->execute(array(':id' => $id));
-        while ($row = $getName->fetch(PDO::FETCH_ASSOC)) {
-            $name = $row['boy_name'];
-        }
+        // $getName = $db->prepare("SELECT boy_name FROM boy_names WHERE id = :id");
+        // $getName->execute(array(':id' => $id));
+        // while ($row = $getName->fetch(PDO::FETCH_ASSOC)) {
+        //     $name = $row['boy_name'];
+        // }
 
-        $insert = $db->prepare("INSERT INTO picked_names(picked_name, login_id) VAlUES (:picked_name, :login_id");
-        $insert->execute(array(':picked_name' => $name, ':login_id' => $userId));
+        // $insert = $db->prepare("INSERT INTO picked_names(picked_name, login_id) VAlUES (:picked_name, :login_id");
+        // $insert->execute(array(':picked_name' => $name, ':login_id' => $userId));
 
         echo "Name: " . $name . " " . "user id: " . $userId . "<br><br><br>";
         //header('location:history.back()');
