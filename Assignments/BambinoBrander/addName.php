2fe1db0c351db4cca6b3f4c11bf1b7f86e646882
meganallred <meg21allred@gmail.com>
2020-10-24 10:16:46 -0400

    testing add boy name

--- Assignments/BambinoBrander/addName.php
+++ Assignments/BambinoBrander/addName.php
@@ -7,6 +7,9 @@
     $name;
     $checkForName;
     
+    echo "name id: " . $id;
+    echo "<br>";
+    
     if (isset($_SESSION['userName'])) {
         $getName = $db->prepare("SELECT boy_name FROM boy_names WHERE id = :id");
         $getName->execute(array(':id' => $id));
@@ -19,14 +22,14 @@
         while ($row = $checkName->fetch(PDO::FETCH_ASSOC)) {
             $checkForName = $row['picked_name'];
         }
-       
+       echo "name: " . $name . " " . "checked name: " . $checkForName;
 
         if($checkForName != $name){
             $insert = $db->prepare("INSERT INTO picked_names(picked_name, login_id) VAlUES (:picked_name, :login_id)");
             $insert->execute(array(':picked_name' => $name, ':login_id' => $userId));
         } 
         
-       header('Location: ' . $_SERVER['HTTP_REFERER']);
+       //header('Location: ' . $_SERVER['HTTP_REFERER']);
     } else {
         header('location:login.php');
     }
