208bd00e6371ac41f06767aa44aa62a77dfbc49d
meganallred <meg21allred@gmail.com>
2020-10-24 10:39:47 -0400

    testing delete

--- Assignments/BambinoBrander/deleteList.php
+++ Assignments/BambinoBrander/deleteList.php
@@ -4,9 +4,11 @@
     $userName = $_SESSION['userName'];
     $userId = $_SESSION['userId'];
 
+    echo "user id: " . $userId;
+
     $deleteList = $db->prepare("DELETE FROM picked_names WHERE login_id = :login_id");
     $deleteList->execute(array(':login_id' => $userId));
 
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    //header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 ?>
