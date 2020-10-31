56c2787b5a01e32a96ab59d848c8d5d4fc9934ef
meganallred <meg21allred@gmail.com>
2020-10-31 19:13:41 -0400

    getting compare to load

--- Assignments/BambinoBrander/compare.php
+++ Assignments/BambinoBrander/compare.php
@@ -2,37 +2,38 @@
     require_once("config.php"); 
     session_start();
 
+   echo " This is compare.php!";
 
-    $compUser = $_POST['userName'];
-    $compEmail = $_POST['userEmail'];
-    $compData = array();
-    $compUserId;
+    // $compUser = $_POST['userName'];
+    // $compEmail = $_POST['userEmail'];
+    // $compData = array();
+    // $compUserId;
 
-    $checkCompUser = $db->prepare("SELECT * FROM user_login Where userName = :userName and userEmail = :userEmail");
-    $checkCompUser->execute(array(':userName' => $compUser, ':userEmail' => $compEmail));
+    // $checkCompUser = $db->prepare("SELECT * FROM user_login Where userName = :userName and userEmail = :userEmail");
+    // $checkCompUser->execute(array(':userName' => $compUser, ':userEmail' => $compEmail));
 
-    while ($row = $checkCompUser->fetch(PDO::FETCH_ASSOC)) {
-         $compUserId = $row['id']; 
-         $num++;  
-    }
+    // while ($row = $checkCompUser->fetch(PDO::FETCH_ASSOC)) {
+    //      $compUserId = $row['id']; 
+    //      $num++;  
+    // }
 
-    if($num == 1) {
-        echo "user found";
+    // if($num == 1) {
+    //     echo "user found";
 
-        $getCompUserList = $db->prepare("SELECT picked_name FROM user_login Where login_id = :compUserId");
-        $getCompUserList->execute(array(':compUserId' => $compUserId));
+    //     $getCompUserList = $db->prepare("SELECT picked_name FROM user_login Where login_id = :compUserId");
+    //     $getCompUserList->execute(array(':compUserId' => $compUserId));
 
-        echo "<ul>";
-        while ($row = $getCompUserList->fetch(PDO::FETCH_ASSOC)) {
-            echo "<li class='nameList'>- " . $row['picked_name'] . "</li>";
-            }
-        echo "</ul>";
+    //     echo "<ul>";
+    //     while ($row = $getCompUserList->fetch(PDO::FETCH_ASSOC)) {
+    //         echo "<li class='nameList'>- " . $row['picked_name'] . "</li>";
+    //         }
+    //     echo "</ul>";
       
-    } else {
-        echo "user not found, please try again.";
-        echo '<input class="btn boy" type="button" value="try again" onClick="location.href=enterCompareLogin.php">';
+    // } else {
+    //     echo "user not found, please try again.";
+    //     echo '<input class="btn boy" type="button" value="try again" onClick="location.href=enterCompareLogin.php">';
 
-    }
+    // }
 ?>
 
 
--- Assignments/BambinoBrander/index.php
+++ Assignments/BambinoBrander/index.php
@@ -19,6 +19,7 @@ $userName = $_SESSION['userName'];
                 <div>
                     <ul class="nav">
                     <a href="showNames.php"><li>LIST</li></a>
+                    <a href="enterCompareLogin.php"><li>COMPARE LISTS</li></a>
                     <a href="index.php"><li>HOME</li></a>
                     <?php if (isset($_SESSION['userName'])) {
                         echo "<a href='logout.php'><li>LOG OUT</li></a>";
